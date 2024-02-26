f3e30dc2081329da8b09961798df6fb270bbd780
ori raisfeld <raisfeldori@gmail.com>
2024-02-26 13:07:30 +0200

    initial version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,11 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+fn log(text: &str) {println!("{}", text);}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![log])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
