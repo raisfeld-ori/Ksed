bcd6009530a591c269466243913e30350ecd2fe9
ori raisfeld <raisfeldori@gmail.com>
2024-02-28 22:04:20 +0200

    worked on the login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,13 +1,22 @@
 import './login.css';
 import { window } from '@tauri-apps/api';
+import { useState } from 'react';
 
 function login(){
-
     return <div id='background'>
-        <button onClick={() => window.appWindow.setFullscreen(false)}>
-            minimize the screen
+        <button onClick={() => window.appWindow.close()} style={{'position': "absolute"}}>
+            X
         </button>
-        <div className='triangle'></div>
+        <div className='triangle down'></div>
+        <div className='triangle left'></div>
+        <div id='menu'>
+            <button>login</button>
+            <button>sign up</button>
+
+            <form>
+                <button>log in</button>
+            </form>
+        </div>
     </div>
 }
 
