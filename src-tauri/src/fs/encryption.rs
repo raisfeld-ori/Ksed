d2c3289180f85515c77d44e26dee063533de5402
aviv-segal <avivsegal2008@gmail.com>
2024-03-03 11:09:02 +0200

    worked on encryption

--- src-tauri/src/fs/encryption.rs
+++ src-tauri/src/fs/encryption.rs
@@ -2,12 +2,42 @@ use crypto::{aes, blockmodes};
 use crypto::buffer::{BufferResult, ReadBuffer, RefReadBuffer, RefWriteBuffer, WriteBuffer};
 use crypto::aes::cbc_decryptor;
 
-pub fn decrypt(name: &str, password: &str, data: Vec<u8>) -> Vec<u8> {
-    // the key and the iv should be 16 bytes
-    let key = password.as_bytes();
-    let iv = name.as_bytes();
+pub fn encrypt(username: &str, password: &str, data: Vec<u8>) -> Vec<u8> {
+  // The key and the iv have to be 16 bytes!!!
+  let key = password.as_bytes();
+  let iv = padding(username, password);
 
-    let mut cipher = cbc_decryptor(aes::KeySize::KeySize128, key, iv, blockmodes::PkcsPadding);
+  let mut cipher = aes::cbc_encryptor(aes::KeySize::KeySize128, key, &iv, blockmodes::PkcsPadding);
+
+  let mut read_buffer = RefReadBuffer::new(&data);
+  let mut buffer = [0; 4096];
+  let mut write_buffer = RefWriteBuffer::new(&mut buffer);
+
+  let mut final_result = Vec::<u8>::new();
+
+  loop {
+    let encryption_result = cipher.encrypt(&mut read_buffer, &mut write_buffer, true);
+    
+    match encryption_result {
+        Ok(BufferResult::BufferUnderflow) => break,
+        Ok(BufferResult::BufferOverflow) => {
+          final_result.extend(write_buffer.take_read_buffer().take_remaining());
+        }
+        Err(e) => panic!("{:?}", e),
+    }
+  }
+  final_result.extend(write_buffer.take_read_buffer().take_remaining());
+  return final_result;
+  
+  
+}
+
+pub fn decrypt(username: &str, password: &str, data: Vec<u8>) -> Vec<u8> {
+    // The key and the iv have to be 16 bytes!!!
+    let key = password.as_bytes();
+    let iv = padding(username, password);
+
+    let mut cipher = cbc_decryptor(aes::KeySize::KeySize128, key, &iv, blockmodes::PkcsPadding);
 
     let mut read_buffer = RefReadBuffer::new(&data);
     let mut buffer = [0; 4096];
@@ -29,46 +59,38 @@ pub fn decrypt(name: &str, password: &str, data: Vec<u8>) -> Vec<u8> {
 
     final_result.extend(write_buffer.take_read_buffer().take_remaining());
 
-    final_result
+    return final_result
 }
 
 
-pub fn encrypt(name: &str, password: &str, data: Vec<u8>) 
--> Vec<u8> {
-  let key = password.as_bytes();
-  let iv = name.as_bytes();
 
-  let mut cipher = aes::cbc_encryptor(aes::KeySize::KeySize128, key, iv, blockmodes::PkcsPadding);
+fn padding(username: &str, password: &str) -> Vec<u8> {
+  let mut padded_username = username.chars().collect::<Vec<char>>();
+  let password_chars = password.chars().rev().collect::<Vec<char>>();
 
-  let mut read_buffer = RefReadBuffer::new(&data);
-  let mut buffer = [0; 4096];
-  let mut write_buffer = RefWriteBuffer::new(&mut buffer);
-
-  let mut final_result = Vec::<u8>::new();
-
-  loop {
-    let encryption_result = cipher.encrypt(&mut read_buffer, &mut write_buffer, true);
-    
-    match encryption_result {
-        Ok(BufferResult::BufferUnderflow) => break,
-        Ok(BufferResult::BufferOverflow) => {
-          final_result.extend(write_buffer.take_read_buffer().take_remaining());
-        }
-        Err(e) => panic!("{:?}", e),
-    }
+  while padded_username.len() < 16 {
+      if password_chars.len() > 0 {
+          padded_username.push(password_chars[padded_username.len() % password_chars.len()]);
+      } else {
+          break;
+      }
   }
-  final_result.extend(write_buffer.take_read_buffer().take_remaining());
-  return  final_result;
-  
-  
+  // Convert the padded username and password to byte vectors
+  let padded_username_bytes = padded_username.into_iter().map(|c| c as u8).collect::<Vec<u8>>();
+  padded_username_bytes
 }
+
 
 #[test]
 fn test_encryption(){
-  let data = "secret";
+  let username = "aviv";
+  let password = "digmas5avivsegal";
+  let data = "secret messagess".as_bytes();
+  let result2 = encrypt(username, password, data.to_vec());
+  assert_ne!(data, result2);
+  let result3 = decrypt(username, password, result2);
+  assert_eq!(data, result3);
 
-  let result = encrypt("aviv", "digmas", data.as_bytes().to_vec());
-  assert_ne!(data.as_bytes().to_vec(), result);
-  let result = decrypt("aviv", "digmas5", result);
-  assert_eq!(data.as_bytes().to_vec(), result);
+  
+  
 }
