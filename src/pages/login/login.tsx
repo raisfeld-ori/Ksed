32e68acb9c8487ec7c49c497ab4a7b653b0f907a
Fravy <arirl.elkaslasi@gmail.com>
2024-03-03 13:03:42 +0200

    gridset :D

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import { invoke } from '@tauri-apps/api';
 import { useNavigate } from 'react-router-dom';
 
+
+
+
 function login(){
     const [name, setname] = useState("");
     const [password, setpassword] = useState("");
@@ -19,26 +22,41 @@ function login(){
         }
     }
 
-    return <div id='background'>
-        <div className='triangle down'></div>
-        <div className='triangle left'></div>
-        <div id='menu'>
-            <button className='upperbutton' aria-current={true} onClick={() => navigate("/")}>login</button>
-            <button className='upperbutton' aria-current={false} onClick={() => navigate("/signup")}>sign up</button>
 
-            <div id='form'>
-                <h1>Welcome back!</h1>
-                <p>please log in</p>
-                <input onChange={e => setname(e.currentTarget.value)} placeholder='write your name'></input>
-                <br />
-                <input onChange={e => setpassword(e.currentTarget.value)} placeholder='write your password'></input>
-                <br />
-                <button onClick={authenticate}>log in</button>
-                <br />
-                <p id='error'>{error}</p>
+
+        return <div id='background'>
+            
+        <div className='square'></div> 
+        <button className='upperbutton' aria-current={true} onClick={() => navigate("/")}>Login</button>
+                <button className='upperbutton' aria-current={false} onClick={() => navigate("/signup")}>Sign Up</button>
+            <div className='triangle down'></div>   
+            <div className='triangle left'></div>
+            <div id='menu'>
+
+                <div id='form'>
+
+                    <h1 id='welcome_login'>Login</h1>
+                    <p id='plslogin'>Welcome back! <td></td> please log to your account</p>
+
+                    <div className="input-group">
+                    <label className="label">Username</label>
+                     <input onChange={e => setname(e.currentTarget.value)} autoComplete="off" name="info" id="info" className="input" placeholder='Enter your username' type="info" />
+                    <div ></div>
+                     </div>
+
+                     <div className="input-group">
+                    <label className="label">Password</label>
+                     <input onChange={e => setpassword(e.currentTarget.value)} autoComplete="off" name="info" id="info" className="input" placeholder='Enter your password' type="info" />
+                    <div ></div>
+                     </div>
+                    <button className='signup_login_button_main_menu' onClick={authenticate}>Log In</button>
+                    <br />
+                    <p id='error'>{error}</p>
+                    
+                </div>
             </div>
         </div>
-    </div>
-}
+        
+    }
 
 export default login;
