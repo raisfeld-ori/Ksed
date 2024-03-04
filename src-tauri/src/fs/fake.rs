b9aab0db77a73cc2ba3e434d13041ce80b4c7d7a
ori raisfeld <raisfeldori@gmail.com>
2024-03-04 10:42:42 +0200

    worked on the fake file system

--- src-tauri/src/fs/fake.rs
+++ src-tauri/src/fs/fake.rs
@@ -8,13 +8,18 @@ pub struct File{
     pub extension: String,
     data: Vec<u8>,
 }
-
+#[derive(Debug, Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Directory{
     pub name: String,
     pub directories: Box<Vec<Directory>>,
     pub files: Vec<File>,
 }
 
+impl Directory{
+    pub fn new(name: String) -> Self{Self{name, directories: Box::new(Vec::new()), files: Vec::new()}}
+}
+
+#[derive(Debug, Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Home{
     pub directories: Vec<Directory>,
     pub files: Vec<File>,
@@ -23,4 +28,5 @@ pub struct Home{
 impl Home{
     pub const fn new() -> Self{Self { directories: Vec::new(), files: Vec::new() }}
     pub fn name(&self) -> String {String::from("Home")}
+    pub fn mkdir(&mut self, dir: String) {self.directories.push(Directory::new(dir))}
 }
