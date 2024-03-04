9138a0b480a3915e2ff8a7ff4657d09cc30078c1
ori raisfeld <raisfeldori@gmail.com>
2024-03-04 10:36:16 +0200

    created the Home struct

--- src-tauri/src/fs/fake.rs
+++ src-tauri/src/fs/fake.rs
@@ -1,5 +1,7 @@
 use serde::{Serialize, Deserialize};
 
+pub static mut FS: Home = Home::new();
+
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, PartialOrd)]
 pub struct File{
     pub name: String,
@@ -11,5 +13,14 @@ pub struct Directory{
     pub name: String,
     pub directories: Box<Vec<Directory>>,
     pub files: Vec<File>,
+}
 
+pub struct Home{
+    pub directories: Vec<Directory>,
+    pub files: Vec<File>,
+}
+
+impl Home{
+    pub const fn new() -> Self{Self { directories: Vec::new(), files: Vec::new() }}
+    pub fn name(&self) -> String {String::from("Home")}
 }
