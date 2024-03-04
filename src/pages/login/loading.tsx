abef4da906186612cecd6f422ba5aab3894b3543
ori raisfeld <raisfeldori@gmail.com>
2024-03-04 13:27:48 +0200

    added ico icon

--- src/pages/login/loading.tsx
+++ src/pages/login/loading.tsx
@@ -6,19 +6,16 @@ import { useLocation } from 'react-router-dom';
 
 // loading the page requires a username and a password
 function loading(){
-    const [value, setvalue] = useState(0);
     const location = useLocation();
     if (!location.state){return <h1>DONT ACCESS THIS PAGE WITHOUT LOGGING IN</h1>}
-    setvalue(1);
-    document.addEventListener('rust_event', () => {console.log("test, ", value)});
+    document.addEventListener('rust_event', () => {});
     invoke("update", {});
     invoke("update", {});
     return <div id='background'>
-        {value}
         <div id='user'>
             <h1>welcome {location.state.name}!</h1>
             <p>pfp will be here</p>
-            <progress max={10} value={1}>{value}</progress>
+            <progress max={10} value={1}></progress>
         </div>
     </div>
 }
