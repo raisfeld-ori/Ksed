22be55e61077020706d36b8398675f6b542517f5
ori raisfeld <raisfeldori@gmail.com>
2024-03-05 10:04:16 +0200

    finished save_user

--- src-tauri/src/data/auth.rs
+++ src-tauri/src/data/auth.rs
@@ -2,8 +2,9 @@ use crate::dir;
 use crate::fs::encryption::{aes_encrypt, aes_decrypt};
 use crate::data::json::data_bytes;
 use crate::data::json::init_user_data;
-use std::fs::{create_dir, read_dir, write};
-use std::path::Path;
+use std::fs::{create_dir, read_dir, File};
+use std::io::Write;
+use std::path::{Path, PathBuf};
 use base64::{encode, decode};
 use serde_json::Value;
 use tauri::{Manager, Runtime};
@@ -19,6 +20,12 @@ pub fn authenticate_user(name: &str, password: &str) -> bool {
 }
 
 #[tauri::command]
+async fn load_user<R: Runtime>(app: tauri::AppHandle<R>, window: tauri::Window<R>) -> Result<(), String> {
+    
+    Ok(())
+}
+
+#[tauri::command]
 pub fn save_user(name: &str, password: &str){
     let location: &[u8] = &aes_encrypt(name, password, name.as_bytes());
     let location = dir().join(format!("{:?}", location));
@@ -26,9 +33,19 @@ pub fn save_user(name: &str, password: &str){
     let data_0: &[u8] = &data.0;
     let data_1: &[u8] = &data.1;
     let encrypted_user_data = aes_encrypt(name, password, data_0);
+    let encrypted_user_name = format!("{:?}", aes_encrypt(name, password, b"user data"));
     let encrypted_system_data = aes_encrypt(name, password, data_1);
+    let encrypted_system_name = format!("{:?}", aes_encrypt(name, password, b"system data"));
+    create_dir(&location).expect("could not create a directory");
     println!("{:?}", location);
-    //create_dir(location).expect("could not create a directory");
+    File::create(location.join(encrypted_user_name))
+    .expect("failed to create a file")
+    .write_all(&encrypted_user_data)
+    .expect("failed to write data into file");
+    File::create(location.join(encrypted_system_name))
+    .expect("failed to create a file")
+    .write_all(&encrypted_system_data)
+    .expect("failed to write data into file");
 }
 
 #[test]
--- src/pages/login/loading.tsx
+++ src/pages/login/loading.tsx
@@ -1,17 +1,19 @@
 import './login.css';
 import { invoke } from '@tauri-apps/api';
+import { useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 // loading the page requires a username and a password
 function loading(){
+    const [value, setvalue] = useState(0);
     const location = useLocation();
     if (!location.state){return <h1>DONT ACCESS THIS PAGE WITHOUT LOGGING IN</h1>}
     document.addEventListener('rust_event', () => {});
-    invoke("update", {});
-    invoke("update", {});
+    invoke("user_", location.state);
     return <div id='background'>
         <div id='user'>
+            <p>{value}</p>
             <h1>welcome {location.state.name}!</h1>
             <p>pfp will be here</p>
             <progress max={10} value={1}></progress>
