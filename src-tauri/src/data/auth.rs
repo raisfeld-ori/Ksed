b2429a445d9ab3ff84bc79b18cbb06e42df9de4e
ori raisfeld <raisfeldori@gmail.com>
2024-03-05 11:22:42 +0200

    worked on windows permissions

--- src-tauri/src/data/auth.rs
+++ src-tauri/src/data/auth.rs
@@ -1,11 +1,10 @@
-use crate::dir;
-use crate::fs::encryption::{aes_encrypt, aes_decrypt};
+use crate::{dir, request_permissions};
+use crate::fs::encryption::{aes_encrypt, aes_decrypt, aes_try_decrypt};
 use crate::data::json::data_bytes;
 use crate::data::json::init_user_data;
 use std::fs::{create_dir, read_dir, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use base64::{encode, decode};
 use serde_json::Value;
 use tauri::{Manager, Runtime};
 
@@ -15,7 +14,10 @@ pub fn update<R: Runtime>(app: tauri::AppHandle<R>) {app.trigger_global("rust_ev
 
 #[tauri::command]
 pub fn authenticate_user(name: &str, password: &str) -> bool {
-    // for now, authentication is not possible since decryption is not ready
+    let location: &[u8] = &aes_encrypt(name, password, name.as_bytes());
+    let location = dir().join(format!("{:?}", location));
+    println!("{:?}", location.read_dir().iter().nth(1));
+
     return true;
 }
 
@@ -36,16 +38,31 @@ pub fn save_user(name: &str, password: &str){
     let encrypted_user_name = format!("{:?}", aes_encrypt(name, password, b"user data"));
     let encrypted_system_data = aes_encrypt(name, password, data_1);
     let encrypted_system_name = format!("{:?}", aes_encrypt(name, password, b"system data"));
-    create_dir(&location).expect("could not create a directory");
-    println!("{:?}", location);
-    File::create(location.join(encrypted_user_name))
-    .expect("failed to create a file")
-    .write_all(&encrypted_user_data)
-    .expect("failed to write data into file");
-    File::create(location.join(encrypted_system_name))
-    .expect("failed to create a file")
-    .write_all(&encrypted_system_data)
-    .expect("failed to write data into file");
+    if !location.exists(){create_dir(&location).expect("could not create a directory");}
+    if location.join(&encrypted_user_name).exists(){
+        File::open(location.join(encrypted_user_name))
+        .expect("failed to open an existing file")
+        .write_all(&encrypted_user_data)
+        .expect("failed to write data");
+    }
+    else{ 
+        File::create(location.join(encrypted_user_name))
+        .expect("failed to create a file")
+        .write_all(&encrypted_user_data)
+        .expect("failed to write data into file");
+    }
+    if location.join(&encrypted_system_name).exists(){
+        File::open(location.join(encrypted_system_name))
+        .expect("failed to open an existing file")
+        .write_all(&encrypted_system_data)
+        .expect("failed to write data");
+    }
+    else{ 
+        File::create(location.join(encrypted_system_name))
+        .expect("failed to create a file")
+        .write_all(&encrypted_system_data)
+        .expect("failed to write data into file");
+    }
 }
 
 #[test]
@@ -55,4 +72,5 @@ fn test_authentication(){
     let name = "test";
     let password = "test";
     save_user(name, password);
+    authenticate_user(name, password);
 }
