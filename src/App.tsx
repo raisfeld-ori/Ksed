565ab80ad39e8f244ed0e5a0c6ee45f1b1fde1bf
ori raisfeld <raisfeldori@gmail.com>
2024-03-06 11:27:57 +0200

    starting the main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import login from './pages/login/login';
 import signup from './pages/login/signup';
 import { Route, Routes } from "react-router-dom";
 import Loading from "./pages/login/loading";
+import main_page from './pages/main_page/main_page';
 
 function App() {
   const init = () => useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" element={login()}></Route>
       <Route path="/signup" element={signup()}></Route>
       <Route path="/loading" element={<Loading />}></Route>
+      <Route path="/main_page" element={main_page()}></Route>
   </Routes>
 }
 
---
+++ src/pages/main_page/main_page.tsx
@@ -0,0 +1,11 @@
+import './main_page.css';
+
+
+export default function main_page(){
+
+
+    return <div id='background'>
+        <div id='desktop'></div>
+        <div id='taskbar'></div>
+    </div>
+}
