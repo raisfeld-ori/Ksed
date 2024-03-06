a1d2b006a81bf36cbd417c2d57533a5e62a5e4b5
ori raisfeld <raisfeldori@gmail.com>
2024-03-06 11:53:25 +0200

    working on the desktop and apps

--- src/pages/main_page/main_page.tsx
+++ src/pages/main_page/main_page.tsx
@@ -1,11 +1,13 @@
 import './main_page.css';
-
+import App from './make_app';
 
 export default function main_page(){
 
 
     return <div id='background'>
-        <div id='desktop'></div>
+        <div id='desktop'>
+            <App />
+        </div>
         <div id='taskbar'></div>
     </div>
 }
