bca6c3abb2420720fa2b50c13771c713a6463be4
Fravy <arirl.elkaslasi@gmail.com>
2024-03-06 13:02:39 +0200

    mainpagestart

--- src/pages/main_page/main_page.tsx
+++ src/pages/main_page/main_page.tsx
@@ -1,17 +1,25 @@
 import './main_page.css';
-import miku2 from '../../assets/avfe23a6b11d1ff4752fd.png'
+import miku2 from '../../assets/25694.png'
+import miku3 from '../../assets/search-icon.svg'
+
 
 export default function main_page(){
 
 
     return <div id='background'>
-        <div id='desktop'></div>
-        <div id='taskbar'></div>
 
-        <nav className='navbarmain'>
+        <nav className='navbar'>
+        <button>
+            <img className='homeimg' src={miku2} alt="" />
+        </button>
             <button>
-                <img src={miku2} alt="" />
+                <img className='searchimg' src={miku3} alt="" />
             </button>
+         <p className='time'>12:09 AM</p>
+         <p className='time'>12/10/2027</p>
+
+
+
         </nav>
     </div>
 }
