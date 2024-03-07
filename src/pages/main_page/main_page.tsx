a854eb5d8e91e9c083bc39632b79e3f7f5d491c9
ori raisfeld <raisfeldori@gmail.com>
2024-03-07 18:50:09 +0200

    made a working grid

--- src/pages/main_page/main_page.tsx
+++ src/pages/main_page/main_page.tsx
@@ -1,19 +1,34 @@
 import './main_page.css';
-/*
-this is for the grid, FUCK THE GRID! IT FUCKING SUCKS
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useDrag } from 'react-dnd';
-import ReactGridLayout from 'react-grid-layout';
-import { useDrop } from 'react-dnd';
-*/
 import './main_page.css';
+import { useDraggable } from './Grid';
 import miku2 from '../../assets/25694.png';
 import miku3 from '../../assets/search-icon.svg';
 
-
 export default function main_page(){
+    const [draggableRef, dx, dy] = useDraggable({
+        gridSize: 40,
+    });
+
+    const setDraggableRef = (node: HTMLDivElement | null) => {
+        if (node !== null) {
+            // idk how to fix this, but as long as it doesn't work, the unit testing wont run...
+            draggableRef(node);
+        }
+    };
+
     return <div id='background'>
+
+            <div className="container">
+            <div
+                className="draggable"
+                ref={draggableRef}
+                style={{
+                    transform: `translate3d(${dx}px, ${dy}px, 0)`,
+                }}
+            >
+                Drag me
+            </div>
+        </div>
 
         <nav className='navbar'>
         <button>
@@ -24,9 +39,6 @@ export default function main_page(){
             </button>
          <p className='time'>12:09 AM</p>
          <p className='time'>12/10/2027</p>
-
-
-
         </nav>
     </div>
 }
