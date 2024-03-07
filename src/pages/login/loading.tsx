1fa78e2d1f8624ea8b35a0def30ac6adfd23afa4
ori raisfeld <raisfeldori@gmail.com>
2024-03-07 18:50:09 +0200

    made a working grid

--- src/pages/login/loading.tsx
+++ src/pages/login/loading.tsx
@@ -1,17 +1,14 @@
 import './login.css';
 import { invoke } from '@tauri-apps/api';
-import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import miku from '../../assets/Default_pfp.svg.png';
 
 
 // loading the page requires a username and a password
 function loading(){
-    const [value, setvalue] = useState(0);
     const location = useLocation();
     if (!location.state){return <h1>DONT ACCESS THIS PAGE WITHOUT LOGGING IN</h1>}
     document.addEventListener('rust_event', () => {invoke("printf", {value: "yiipee"})});
-    function run_loading(){invoke("load_user", location.state);}
     document.addEventListener('rust_event', () => {});
     invoke("user_", location.state);
 
