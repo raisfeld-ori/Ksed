f79c99e2f43a259c230246c8106a895421f11cc6
ori raisfeld <raisfeldori@gmail.com>
2024-03-09 11:19:52 +0200

    worked on the grid

--- src/pages/login/loading.tsx
+++ src/pages/login/loading.tsx
@@ -1,7 +1,7 @@
 import './login.css';
 import { invoke } from '@tauri-apps/api';
 import { useLocation } from 'react-router-dom';
-import miku from '../../assets/Default_pfp.svg.png';
+import pfp from './assets/Default_pfp.svg.png';
 
 
 // loading the page requires a username and a password
@@ -19,7 +19,7 @@ function loading(){
         <div className='triangle left'></div>
 
         <div className="dot">
-            <img src={miku} alt="Descriptive text" />
+            <img src={pfp} alt="Descriptive text" />
                 </div>
 
                 <p id='melody'>Welcome {location.state.name}!</p>
--- src/pages/main_page/main_page.tsx
+++ src/pages/main_page/main_page.tsx
@@ -2,23 +2,19 @@ import './main_page.css';
 import './main_page.css';
 import { desktop_app } from './Grid';
 import Grid from './Grid';
-
-import miku2 from '../../assets/25694.png';
-import miku3 from '../../assets/search-icon.svg';
+import folder from './assets/folder-line-icon.svg';
+import menu_icon from './assets/laptop-icon.svg';
+import search from './assets/search-icon.svg';
 
 export default function main_page(){
-    const example_app = desktop_app("example app", miku3);
-    const not_example_app = desktop_app("not example app", miku3);
+    const example_app = desktop_app("example app", folder);
+    const not_example_app = desktop_app("not example app", search);
 
     return <div id='background'>
         <Grid apps={[example_app, not_example_app]} gridSize={50} margin={100}/>
         <nav className='navbar'>
-        <button>
-            <img className='homeimg' src={miku2} alt="" />
-        </button>
-            <button>
-                <img className='searchimg' src={miku3} alt="" />
-            </button>
+        <img className='homeimg' src={menu_icon} alt="" />
+        <img className='searchimg' src={search} alt="" />
          <p className='time'>12:09 AM</p>
          <p className='time'>12/10/2027</p>
         </nav>
