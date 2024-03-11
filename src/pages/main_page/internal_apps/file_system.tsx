f387acec64b0888e35da4cf8d6c238247827b4b4
Fravy <arirl.elkaslasi@gmail.com>
2024-03-11 11:37:16 +0200

    IT ONLY TOOK ONE PUNCH

--- src/pages/main_page/internal_apps/file_system.tsx
+++ src/pages/main_page/internal_apps/file_system.tsx
@@ -2,8 +2,8 @@ import App from './App';
 
 function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Element>>){
     return () => {
-    let app_html = <h1>here is the actual page</h1>;
-    let app = <App element={app_html}/>;
+    let app_html = <h1>here is some code</h1>;
+    let app = <App element={app_html} name='feet pics'/>;
     set_fs_html(app);
 };
 }
