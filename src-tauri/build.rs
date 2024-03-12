f943b877c220db1722b8ef2a849aa2d9b8799f1a
ori raisfeld <raisfeldori@gmail.com>
2024-03-12 10:54:01 +0200

    fixed issues on windows

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,26 +1,4 @@
-
-
 fn main() {
-  let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or("unknown OS".to_string());
-  match target_os.as_str() {
-    "windows" => {
-    let mut res = tauri_winres::WindowsResource::new();
-    res.set_icon("./icons/icon.ico");
-    res.compile().unwrap();
-    },
-    "linux" => {
-        println!("Hello, Root-World!");
-    }
-    _ => {},
-  }
-
-  let resource_file = "d_vault/src-tauri/resource.rc";
-  let macros = vec![
-        "MY_MACRO",
-        "MY_OTHER_MACRO=VALUE",
-    ];
     tauri_build::build();
-    embed_resource::compile(resource_file, macros);
-    
 }
 
--- src/pages/main_page/internal_apps/file_system.tsx
+++ src/pages/main_page/internal_apps/file_system.tsx
@@ -1,9 +1,14 @@
 import App from './App';
 import { invoke } from '@tauri-apps/api';
+import { useState } from 'react';
 
 function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Element>>){
+    const [location, set_location] = useState("");
+    const [files, set_files] = useState([]);
     return () => {
-    let app_html = <h1 onClick={async () => {console.log(await invoke("list_commands", {}));}}>here is some code</h1>;
+    let app_html = <div className='file_system' onLoad={() => {console.log("test");}}>
+        <h1></h1>
+    </div>;
     let app = <App element={app_html} name='feet pics'/>;
     set_fs_html(app);
 };
