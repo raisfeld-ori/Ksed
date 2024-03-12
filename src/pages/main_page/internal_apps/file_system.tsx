6cf6b25a00d0cf0582a5705d60aedf5a750e953a
ori raisfeld <raisfeldori@gmail.com>
2024-03-12 10:48:28 +0200

    starting invokes

--- src/pages/main_page/internal_apps/file_system.tsx
+++ src/pages/main_page/internal_apps/file_system.tsx
@@ -1,7 +1,11 @@
 import App from './App';
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api';
 
 function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Element>>){
     return () => {
+    const [files, set_files] = useState([]);
+    async function load_home(){}
     let app_html = <h1>here is some code</h1>;
     let app = <App element={app_html} name='feet pics'/>;
     set_fs_html(app);
