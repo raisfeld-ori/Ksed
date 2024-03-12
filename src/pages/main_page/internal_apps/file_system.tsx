34f40cf3eeaef7dd0753e91f1e34795047d5502a
Fravy <arirl.elkaslasi@gmail.com>
2024-03-12 09:43:38 +0200

    What is so wrong about being with the monsters?

--- src/pages/main_page/internal_apps/file_system.tsx
+++ src/pages/main_page/internal_apps/file_system.tsx
@@ -1,8 +1,9 @@
 import App from './App';
+import { invoke } from '@tauri-apps/api';
 
 function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Element>>){
     return () => {
-    let app_html = <h1>here is some code</h1>;
+    let app_html = <h1 onClick={async () => {console.log(await invoke("list_commands", {}));}}>here is some code</h1>;
     let app = <App element={app_html} name='feet pics'/>;
     set_fs_html(app);
 };
