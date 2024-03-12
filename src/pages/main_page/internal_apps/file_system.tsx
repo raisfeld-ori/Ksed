0655d2ea603e70bb67381f95c7c890d35b3b97e8
ori raisfeld <raisfeldori@gmail.com>
2024-03-12 12:00:24 +0200

    remade the file system

--- src/pages/main_page/internal_apps/file_system.tsx
+++ src/pages/main_page/internal_apps/file_system.tsx
@@ -3,11 +3,17 @@ import { invoke } from '@tauri-apps/api';
 import { useState } from 'react';
 
 function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Element>>){
-    const [location, set_location] = useState("");
+    const [location, set_location] = useState("Home");
     const [files, set_files] = useState([]);
+    //@ts-expect-error
+    invoke("ls", {}).then(result => set_files(result)).catch(console.log);
+    //@ts-expect-error
+    invoke("pwd", {}).then(result => set_location(result)).catch(console.log);
+    console.log(files, ", ", location);
     return () => {
-    let app_html = <div className='file_system' onLoad={() => {console.log("test");}}>
-        <h1></h1>
+    let app_html = <div className='file_system'>
+        <h1>{location}</h1>
+
     </div>;
     let app = <App element={app_html} name='feet pics'/>;
     set_fs_html(app);
