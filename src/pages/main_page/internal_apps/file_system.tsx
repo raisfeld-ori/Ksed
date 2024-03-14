1d752b37550d4060c15f2008d22c1dfb35c5860f
Fravy <arirl.elkaslasi@gmail.com>
2024-03-14 11:45:48 +0200

    Mankind’s greatest fear is Mankind itself.

--- src/pages/main_page/internal_apps/file_system.tsx
+++ src/pages/main_page/internal_apps/file_system.tsx
@@ -2,13 +2,6 @@ import App from './App';
 import { invoke } from '@tauri-apps/api';
 import { useState } from 'react';
 
-function ContextMenu(){
-    
-    return <div>
-        text here
-    </div>
-}
-
 function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Element>>){
     const [location, set_location] = useState("Home");
     const [files, set_files] = useState([]);
@@ -16,17 +9,7 @@ function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Elemen
     invoke("ls", {}).then(result => set_files(result)).catch(console.log);
     //@ts-expect-error
     invoke("pwd", {}).then(result => set_location(result)).catch(console.log);
-
-    const [menu_open, set_menu] = useState(false);
-    const right_click = (ev: MouseEvent) => {
-        //set_context({x: ev.clientX, y: ev.clientY});
-        console.log("right clicked");
-        //set_menu(true);
-
-    }
-    let internal_id = 0;
-    function id(){return (internal_id + 1).toString();}
-    document.addEventListener('contextmenu', right_click);
+    console.log(files, ", ", location);
     return () => {
         let Application = <div className='ApplicationDirectory'>
                     <h1 className='filesystemtxt2'>/{location}/</h1>
@@ -35,14 +18,9 @@ function file_system(set_fs_html: React.Dispatch<React.SetStateAction<JSX.Elemen
     let app_html = <div className='frametest2'>
             {Application}
 
-    let app_html = <div className='frametest2'>
-        <div className='ApplicationDirectory'>
-            <h1 className='filesystemtxt2'>/{location}/</h1>
-        </div>
 
-        {menu_open && <ContextMenu />}
     </div>;
-    let app = <App element={app_html} id={id()}  name='feet pics'/>;
+    let app = <App element={app_html} name='feet pics'/>;
     set_fs_html(app);
 };
 }
