8e6f2edd334dc4909ef925f082113f2328a9f95d
Fravy <arirl.elkaslasi@gmail.com>
2024-03-14 11:45:48 +0200

    Mankind’s greatest fear is Mankind itself.

--- src/pages/main_page/main_page.tsx
+++ src/pages/main_page/main_page.tsx
@@ -2,9 +2,10 @@ import './main_page.css';
 import './main_page.css';
 import { desktop_app } from './Grid';
 import Grid from './Grid';
-import folder from './assets/folder-line-icon.svg';
-import menu_icon from './assets/laptop-icon.svg';
-import search from './assets/search-icon.svg';
+import folder from './assets/folder.png';
+import menu_icon from './assets/computer-laptop-color-icon.webp';
+import search from './assets/search.png';
+import terminal from './assets/Terminal-icon.png';
 import file_system from './internal_apps/file_system';
 import { useState } from 'react';
 
@@ -19,8 +20,8 @@ export default function main_page(){
         <nav className='navbar'>
         <img className='homeimg' src={menu_icon} alt="" />
         <img className='searchimg' src={search} alt="" />
-         <p className='time'>12:09 AM</p>
-         <p className='time'>12/10/2027</p>
+        <img className='terminalimg' src={terminal} alt="" />
+         <p className='time'>12:09 AM <br /> 12/10/2027</p>
         </nav>
     </div>
 }
