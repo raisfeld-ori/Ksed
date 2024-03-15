3ff0388cc44c5fd1087bc334595c44b513395791
ori raisfeld <raisfeldori@gmail.com>
2024-03-15 16:00:43 +0200

    added a ctx menu and a menu

--- src/pages/main_page/internal_apps/file_system.tsx
+++ src/pages/main_page/internal_apps/file_system.tsx
@@ -1,6 +1,6 @@
 import App, {State} from './App';
 import { invoke } from '@tauri-apps/api';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function file_system() : [JSX.Element, React.Dispatch<React.SetStateAction<string>>]{
     const [location, set_location] = useState("Home");
@@ -10,16 +10,35 @@ function file_system() : [JSX.Element, React.Dispatch<React.SetStateAction<strin
     //@ts-expect-error
     invoke("pwd", {}).then(result => set_location(result)).catch(console.log);
 
-    const [menu_open, set_menu] = useState(false);
+    const [{dx, dy}, set_positions] = useState({dx: 0, dy: 0});
+    const [ctx_display, set_ctx_display] = useState('none');
     const right_click = (ev: MouseEvent) => {
         //set_context({x: ev.clientX, y: ev.clientY});
         console.log("right clicked");
         //set_menu(true);
-
+        set_ctx_display('inherit');
+        set_positions({dx: ev.clientX, dy: ev.clientY});
     }
-    document.addEventListener('contextmenu', right_click);
+    const left_click = () =>{
+        if (ctx_display) {set_ctx_display('none');}
+    }
+    useEffect(() => {
+        document.addEventListener('contextmenu', right_click);
+        document.addEventListener('click', left_click);
+        return () => {
+            document.removeEventListener('contextmenu', right_click);
+            document.removeEventListener('click', left_click);
+        };
+    }, []); 
     let Application = <div className='ApplicationDirectory'>
             <h1 className='filesystemtxt2'>/{location}/</h1>
+
+            <div className='ContextMenu'
+            style={{
+                transform: `translate3d(${dx}px, ${dy}px, 0)`,
+                display: `${ctx_display}`,
+            }}
+            ><h1>test</h1></div>
         </div>   
     let app_html = <div className='frametest2'>
     {Application}
--- src/pages/main_page/main_page.tsx
+++ src/pages/main_page/main_page.tsx
@@ -1,7 +1,5 @@
-
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './main_page.css';
-import { State } from './internal_apps/App';
 import Grid from './Grid';
 import folder from './assets/folder.png';
 import menu_icon from './assets/computer-laptop-color-icon.webp';
@@ -17,17 +15,20 @@ export default function MainPage() {
     const example_app = desktop_app("Dolphin", folder, () => {fs_display('inherit')});
     const not_example_app = desktop_app("Search", search, () => { console.log("test"); });
     const terminal = desktop_app("Terminal", terminald, () => {});
-
+    const [menu, set_menu] = useState(false);
     return (
         <div id='background'>
             {app}
             <Grid apps={[example_app, not_example_app, terminal]} gridSize={50} margin={120} />
             <nav className='navbar'>
-                <img className='homeimg' src={menu_icon} alt="" />
+                <img className='homeimg' onClick={() => set_menu(!menu)} src={menu_icon} alt="" />
                 <img className='searchimg' src={search} alt="" />
                 <img className='terminalimg' src={terminald} alt="" />
                 <p className='time'>12:09 AM <br /> 12/10/2027</p>
             </nav>
+            <div className={`menu ${menu ? 'show' : 'hide'}`}>
+                <h1>test</h1>
+            </div>
             <video className='hakari' src={ibetonhakari} width="100%" height="100%" autoPlay muted loop>
     Your browser does not support the video tag.
 </video>
