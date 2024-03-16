d58d10a758968edb4c91d158bf65a0671529660b
Fravy <arirl.elkaslasi@gmail.com>
2024-03-16 15:27:54 +0200

    The System Uses Me, and I Use the System.

--- src/pages/main_page/main_page.tsx
+++ src/pages/main_page/main_page.tsx
@@ -4,7 +4,7 @@ import Grid from './Grid';
 import folder from './assets/folder.png';
 import menu_icon from './assets/computer-laptop-color-icon.webp';
 import search from './assets/search.png';
-import terminald from './assets/Terminal-icon.png';
+import terminald from './assets/terminal.png';
 import file_system from './internal_apps/file_system';
 import ibetonhakari from './assets/TOCA2.mp4';;
 import { desktop_app } from './Grid';
