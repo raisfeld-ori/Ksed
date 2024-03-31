61213c1e7418448c912c4be79a17ed4708ae3b58
ori raisfeld <raisfeldori@gmail.com>
2024-03-31 13:22:02 +0300

    Accurate Quoting
        Advanced costing models to accurately ﬁt your company’s pricing structure and product offering including parts,

--- ksed_website/src/app/page.tsx
+++ ksed_website/src/app/page.tsx
@@ -1,7 +1,10 @@
 import './page.css';
+
 export default function Home() {
   return (
     <main className="main">
+        <div id="options">
+        </div>
       <h1 className="header">Kdes</h1>
       <h2 className="header">your hidden desktop</h2>
 
