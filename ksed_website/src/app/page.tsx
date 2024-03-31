1c19a0aa529d3873d55b6a555b49fa3bbf719f70
Fravy <arirl.elkaslasi@gmail.com>
2024-03-31 13:34:21 +0300

    navbar pain

--- ksed_website/src/app/page.tsx
+++ ksed_website/src/app/page.tsx
@@ -1,9 +1,38 @@
 import './page.css';
+
 export default function Home() {
   return (
     <main className="main">
-      <h1 className="header">Kdes</h1>
-      <h2 className="header">your hidden desktop</h2>
+    <nav>
+      <div className="wrapper">
+        <div className="logo"><a href="#">
+          <img src="logo.png" alt="" />
+          </a></div>
+        <input type="radio" name="slider" id="menu-btn" />
+        <input type="radio" name="slider" id="close-btn" />
+        <ul className="nav-links">
+          <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times"></i></label>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li>
+            <a href="#" className="desktop-item">Purchase</a>
+            <input type="checkbox" id="showDrop" />
+            <label htmlFor="showDrop" className="mobile-item">Purchase</label>
+            <ul className="drop-menu">
+              <li><a href="#">Weekly</a></li>
+              <li><a href="#">Monthly</a></li>
+              <li><a href="#">Yearly</a></li>
+              <li><a href="#">Lifetime</a></li>
+            </ul>
+          </li>
+          <li>
+          </li>
+          <li><a href="#">Feedback</a></li>
+        </ul>
+        <label htmlFor="menu-btn" className="btn menu-btn"><i className="fas fa-bars"></i></label>
+      </div>
+    </nav>
+
 
     </main>
   );
