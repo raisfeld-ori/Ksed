daae2b64a3c2a19bdaefdc6d265bf828038c1086
ori raisfeld <raisfeldori@gmail.com>
2024-04-02 12:52:26 +0300

    Koronde surimuita toko Chi ga nijinde shimiru keredo Hecchara sa sugu naoru kara Ashita ni wa kasabuta dekiru

--- ksed_website/app/routes/_index.tsx
+++ ksed_website/app/routes/_index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
     <div className="main">
       <div className="square">
         <h1 className="text"><span className="K">K</span><span className="sed">sed</span></h1>
-        <h2 className="text">your private, hidden desktop</h2>
+        <h2 className="text">Your hidden desktop</h2>
         <button>learn more</button>
       </div>
       <div className="square">
