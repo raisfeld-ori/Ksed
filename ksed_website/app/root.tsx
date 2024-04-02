7cacab58896907e88c5c7a9bdfef53618c61e19f
ori raisfeld <raisfeldori@gmail.com>
2024-04-02 12:26:11 +0300

    Let us rise up and be thankful for todays child, raid shadow legends.”

--- ksed_website/app/root.tsx
+++ ksed_website/app/root.tsx
@@ -20,9 +20,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
   	<input className="menu-icon" type="checkbox" id="menu-icon" name="menu-icon"/>
     <label htmlFor="menu-icon"></label>
-  	<nav className="nav"> 		
+  	<nav className="nav">
   		<ul className="pt-5">
-  			<li><a href="#">Work</a></li>
+  			<li><a href="#">get back to Work!</a></li>
   			<li><a href="#">Studio</a></li>
   			<li><a href="#">News</a></li>
   			<li><a href="#">Contact</a></li>
