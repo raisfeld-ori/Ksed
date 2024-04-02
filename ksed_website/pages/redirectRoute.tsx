3ce065360a126b9f191dbb1ef4e3d8292bf0298e
ori raisfeld <raisfeldori@gmail.com>
2024-04-02 09:34:13 +0300

    There is nothing more dreadful than the habit of doubt. Doubt separates people. It is a poison that disintegrates friendships and breaks up pleasant relations. It is a thorn that irritates and hurts; it is a sword that kills

--- ksed_website/pages/redirectRoute.tsx
+++
@@ -1,9 +0,0 @@
-import style from './redirectRoute.module.css';
-export default function Default() {
-    return (
-      <div>
-        <h1 className={style.header}>Ksed</h1>
-        <h2 className={style.header}>your hidden personal computer</h2>
-      </div>
-    );
-  }
--- ksed_website/pages/test.tsx
+++
@@ -1,5 +0,0 @@
-
-
-export default function Test(){
-  return <h1>test</h1>
-}
