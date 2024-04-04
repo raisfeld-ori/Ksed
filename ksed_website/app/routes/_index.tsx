fcbc11283a3c34773c417c72d845a786f7dc990f
Fravy <arirl.elkaslasi@gmail.com>
2024-04-04 11:42:58 +0300

    Kimi da yo  kimi nanda yo  egao wo kureta

--- ksed_website/app/routes/_index.tsx
+++ ksed_website/app/routes/_index.tsx
@@ -45,25 +45,40 @@ export default function Index() {
       </div>
       <div className="square">
       <div className="squareinsquare">
-        <div className="exception"><h1>once upon a time</h1></div>
+        <div className="exception"><h1>Questions ✦ Answers</h1></div>
+
+      <div className="navbuttons">
+        <button className="navbutton">Slaves</button>
+        <button className="navbutton">pls money</button>
+        <button className="navbutton">click for cookies</button>
+        <button className="navbutton">Women Rights</button>
+        </div>
         <div className="row">
           <div className="container">
-            <img className="sized" src="literally_me.jpg"></img>
+            <h1 className="sqrtextleftheadhead">Your lie in april</h1>
+            <p className="sqrtextleft">Kimi da yo  kimi nanda yo  egao wo kureta
+               Namida mo hikaru nara  ryuusei ni naru
+                Kizutsuita sono te wo  mou hanasanai de
+            </p>
+            <div className="linesqr"></div>
+            <h1 className="sqrtextlefthead">Your lie in april</h1>
+            <p className="sqrtextleft">Kimi da yo  kimi nanda yo  egao wo kureta
+               Namida mo hikaru nara  ryuusei ni naru
+                Kizutsuita sono te wo  mou hanasanai de
+            </p>
           </div>
           <div className="container">
-            <h1 className="textinsquare">one fool thought he'd find, hoo.</h1>
+            <h1 className="textinsquareheaderheader">one fool thought he'd find, hoo.</h1>
             <p className="textinsquare">
             Purpose in his life along the way
             Don't you run and hide from the truth, you decide
             Everything that lives is gone to waste
             </p>
+            <div className="linesqr2"></div>
+
             <h1 className="textinsquare">Don't you run and hide from the truth</h1>
             <p className="textinsquare">
             you decide Everything that lives is gone to waste
-            Hoo, hoo
-            Hoo, hoo
-            Hoo, hoo
-            Hoo
             Once upon a time, was a fool who thought he'd find
             Purpose in his life along the way
             </p>
