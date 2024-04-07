72fb3fccd360bcc996585ebaf158305cfd19ed79
ori raisfeld <raisfeldori@gmail.com>
2024-04-07 10:24:39 +0300

    That's the problem with drinking, I thought, as I poured myself a drink. If something bad happens you drink in an attempt to forget; if something good happens you drink in order to celebrate; and if nothing happens you drink to make something happen.

--- ksed_website/app/routes/_index.tsx
+++ ksed_website/app/routes/_index.tsx
@@ -31,8 +31,14 @@ export default function Index() {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          if (entry.isIntersecting) {entry.target.classList.add('visible')}
-          else{entry.target.classList.remove('visible')}
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            entry.target.classList.remove('invisible');
+          }
+          else{
+            entry.target.classList.remove('visible');
+            entry.target.classList.add('invisible');
+          }
         });
       },
     );
@@ -64,24 +70,10 @@ export default function Index() {
       </div>
       <div className="square">
         <div className="exception"><h1>what is Ksed?</h1></div>
-        <div className="row">
+        <div className="column">
         <div className="container">
-          <h1 className="text pop">it's your own, hidden desktop</h1>
-          <p className="text pop">
-            do you have important files? or something that you want to hide.
-            it could be that you job requires you to have some important files,
-            or maybe you just have a secret hobby that you want to hide.
-            well, that's what ksed is for! it's a hidden app in your computer,
-            that after being opened, will act like a desktop, except everything you
-            do is hidden, and after finishing, the app will just save the state of
-            the machine the last time you used it and then continue hiding like you
-            didn't even use it.
-          </p> 
-          <h1 className="text">Ksed = desK</h1>
-          <p className="text">
-          応発ヨオ訪合に手41否ひみぱ毒京基をフ由禁はぎふ著5横ユヱ議読合じまん載批よーむリ就保ば浦図マソリ付近ヨ背調帳懸ら。
-          転ゆでぴ菓南ーが統想モレアオ調日わレぞら極指べる並遺フレンき新組づべぽへ崎典ソフムキ購補写ラサオ販理正教わすえ雪球ナケ引自タニツ観候幹び。
-          </p>
+          <h1 className="text pop">your own, hidden desktop</h1>
+          
         </div>
         <div className="container">
           <img className="sized" src="alpha.jpeg"></img>
