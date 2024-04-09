6207c192c1d7409a9969f20b7f7c8a86bc6d4227
ori raisfeld <raisfeldori@gmail.com>
2024-04-09 13:31:14 +0300

    something's in my ass, oooohhhhh

--- ksed_website/app/routes/about.tsx
+++ ksed_website/app/routes/about.tsx
@@ -1,6 +1,32 @@
-
+import './style/about.css';
 
 
 export default function About(){
-    return <h1>ariel quit work!</h1>
+    return <div className="square">
+        <h1 className="text head">about Ksed:</h1>
+        <p className="text body">
+            Underneath the bridge, tarp has sprung a leak
+And the animals I've trapped have all become my pets
+And I'm living off of grass, and the drippings from my ceiling
+It's OK to eat fish 'cause they don't have any feelings
+Something in the way, hmm-mmm
+Something in the way, yeah, hmm-mmm
+Something in the way, hmm-mmm
+Something in the way, yeah, hmm-mmm
+Something in the way, hmm-mmm
+Something in the way, yeah, hmm-mmm
+Underneath the bridge, tarp has sprung a leak
+And the animals I've trapped have all become my pets
+And I'm living off of grass, and the drippings from my ceiling
+It's OK to eat fish 'cause they don't have any feelings
+Something in the way, hmm-mmm
+Something in the way, yeah, hmm-mmm
+Something in the way, hmm-mmm
+Something in the way, yeah, hmm-mmm
+Something in the way, hmm-mmm
+Something in the way, yeah, hmm-mmm
+Something in the way, hmm-mmm
+Something in the way, yeah, hmm-mmm
+</p>
+    </div>
 }
