20947197c88fad614f15f51749f32cc95efd125f
ori raisfeld <raisfeldori@gmail.com>
2024-04-21 02:24:00 +0300

    This is a story of revival … How I was literally able to start walking again … and thinking back, I was constantly praying the whole journey … even this cross-continental race by horse was a journey of prayer. Praying for the weather tomorrow. Praying that when when we wake up the next day there would be a road on the land. Praying that we would have food and a place to sleep. Praying that our campfire would light. And while these common things would repeat, prayer that my friend and the horses would be okay. And we crossed each river one by one. Now … I have crossed the final river.

--- src/pages/main_page/internal_apps/apps/sudo/sudo.tsx
+++ src/pages/main_page/internal_apps/apps/sudo/sudo.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import App, { AppInterface } from "../../App";
 import { invoke } from "@tauri-apps/api";
 import { useNavigate } from "react-router-dom";
