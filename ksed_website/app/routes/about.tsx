0de409de2ec5b85f74f3b772ac620de2251b18d5
Fravy <arirl.elkaslasi@gmail.com>
2024-05-01 10:35:52 +0300

    real!!

--- ksed_website/app/routes/about.tsx
+++ ksed_website/app/routes/about.tsx
@@ -2,5 +2,20 @@
 
 
 export default function About(){
-    return <h1>ariel quit work!</h1>
+    return       <div className="square">
+    <p className="explanation">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Quaerat temporibus a incidunt in error blanditiis sit vero, 
+      ut quisquam perspiciatis, 
+      iste culpa alias beatae aperiam cum dolores, numquam ad accusamus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Ipsam provident autem sapiente quae quibusdam, corporis inventore tenetur beatae 
+      voluptatem nulla commodi itaque neque in earum culpa quia laudantium doloribus! Ex.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ratione dolorum eaque 
+      temporibus asperiores repellat aliquam voluptate iste quas hic
+       deleniti fugit quaerat quisquam quidem, ipsa autem dolor rem deserunt.
+    </p>
+    <div className="justicediv"> <img id="justice" src="alpha.jpeg" alt="" /></div>
+
+</div>
 }
