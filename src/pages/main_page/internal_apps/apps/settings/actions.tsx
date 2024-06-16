9db80a51e6643ee6b431f6a88ea588729485e9f1
ori raisfeld <raisfeldori@gmail.com>
2024-06-16 12:17:17 +0300

    ariel is my rightful owner and i give all of my rights to him

--- src/pages/main_page/internal_apps/apps/settings/actions.tsx
+++ src/pages/main_page/internal_apps/apps/settings/actions.tsx
@@ -2,16 +2,20 @@
 
 export default function CategoryPage(props: {category: string}){
     switch (props.category){
-        case "Welcome": return <div className="main-welcome">
+        case "Welcome": return <div className="settings-page main-welcome">
             <h1>Welcome!</h1>
             <p>
                 this is the welcome page of ksed <br />
             </p>
         </div>
-        case "About": return <div>
+        case "About": return <div className="settings-page">
             <h1>About Ksed</h1>
             <p>something something lorem ipsum</p>
         </div>
         default: return <div>error: unknown category</div>
+        case "Background": return <div className="settings-page">
+            <h1>Set background</h1>
+            <button>nothing here yet):</button>
+        </div>
     }
 }
