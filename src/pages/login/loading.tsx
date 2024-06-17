e8ab313b5c741b10426921e39501c443b862eca4
Fravy <arirl.elkaslasi@gmail.com>
2024-06-17 13:55:13 +0300

    woops forgot to add the navigate after loading

--- src/pages/login/loading.tsx
+++ src/pages/login/loading.tsx
@@ -22,6 +22,8 @@ function loading(){
             await invoke('system_make', {key: 'name',val: name_val});
             let password_val = await invoke('create_value', {valType: 'string', val: location.state.password});
             await invoke('system_make', {key: 'password',val: password_val});
+            navigate('/main_page');
+
         });
     }, []);
 
